593b2bf39e4cdc4ffaa4d5615af8b7a37c026e84
Justin Restivo <justinjrestivo@gmail.com>
2018-01-16 19:13:14 -0600

    Update kernelcheck.sh

--- kernelcheck.sh
+++ kernelcheck.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+from=justin.restivo@citi.com
+to=justin.restivo@citi.com
 kernel_log=/tmp/kernel_log
+
 cd "$(dirname "$0")"
 
 echo > $kernel_log
@@ -9,9 +12,8 @@ if yum check-update | grep kernel
 then
 	echo "Kernel update available at $(date)" 2>&1 | tee -a $kernel_log
 	touch available
-	echo > available
-	echo "From: root <justin@vdimwmonitoring03@nam.nsroot.net>" >> available
-	echo "To: root <justin@vdimwmonitoring03@nam.nsroot.net>" >> available
+	echo "To: $to" >> not-available
+	echo "From: $from" >> available
 	echo "Subject: RHEL Kernel update available" >> available
 	cat $kernel_log >> available
 	sendmail justin.restivo@citi.com < available
@@ -19,9 +21,8 @@ then
 else
 	echo "No kernel update available at $(date)" 2>&1 | tee -a $kernel_log
 	touch not-available
-	echo > not-available
-	echo "From: root <justin@vdimwmonitoring03@nam.nsroot.net>" >> not-available
-	echo "To: root <justin@vdimwmonitoring03@nam.nsroot.net>" >> not-available
+	echo "To: $to" >> not-available
+	echo "From: $from" >> not-available
 	echo "Subject: RHEL Kernel not update available" >> not-available
 	cat $kernel_log >> not-available
 	sendmail justin.restivo@citi.com < not-available
