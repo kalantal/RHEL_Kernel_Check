9c271f09a159b0f7fdd36eae917a214c5dbc162d
Justin Restivo <justinjrestivo@gmail.com>
2018-01-17 16:24:44 -0600

    Update kernelcheck.sh

--- kernelcheck.sh
+++ kernelcheck.sh
@@ -27,36 +27,36 @@ if yum check-update | grep kernel
 then
         echo "Kernel update available at $(date)" 2>&1 | tee -a $kernel_log
         echo >> $kernel_log
-	echo "Current version of RHEL:" >> $kernel_log
-	echo `cat /etc/redhat-release` >> $kernel_log
-	echo `uname -rm` >>$kernel_log
-	echo >> $kernel_log
+		echo "Current version of RHEL:" >> $kernel_log
+		echo `cat /etc/redhat-release` >> $kernel_log
+		echo `uname -r` >>$kernel_log
+		echo >> $kernel_log
         touch available
-	#Build sendmail configuration file
+		#Build sendmail configuration file
         echo "To: $to" >> not-available
         echo "From: $from" >> available
-        echo "Subject: RHEL Kernel update available" >> available
+        echo "Subject: RHEL Kernel update available on system `uname -n`" >> available
         echo >> $kernel_log
         cat $kernel_log >> available
         sendmail justin.restivo@citi.com < available
-	#Cleanup
+		#Cleanup
         rm available
 else
         echo "No kernel update available at $(date)" 2>&1 | tee -a $kernel_log
         echo >> $kernel_log
-	echo "Current version of RHEL:" >> $kernel_log
-	echo `cat /etc/redhat-release` >> $kernel_log
-	echo `uname -rm` >>$kernel_log
+		echo "Current version of RHEL:" >> $kernel_log
+		echo `cat /etc/redhat-release` >> $kernel_log
+		echo `uname -r` >>$kernel_log
         echo >> $kernel_log		
         touch not-available
-	#Build sendmail configuration file
+		#Build sendmail configuration file
         echo "To: $to" >> not-available
         echo "From: $from" >> not-available
-        echo "Subject: RHEL Kernel not update available" >> not-available
+        echo "Subject: No RHEL kernel update available on system `uname -n`" >> not-available
         echo >> $kernel_log
         cat $kernel_log >> not-available
         sendmail justin.restivo@citi.com < not-available
-	#Cleanup
+		#Cleanup
         rm not-available
 fi
 
