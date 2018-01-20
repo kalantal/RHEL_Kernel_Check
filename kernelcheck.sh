1d551ba29134f996f698b3c7d38b49d254e9fcad
Justin Restivo <justinjrestivo@gmail.com>
2018-01-20 12:45:23 -0600

    Update kernelcheck.sh

--- kernelcheck.sh
+++ kernelcheck.sh
@@ -2,7 +2,7 @@
 #set -x
 
 #Change "to" and "from" where appropriate
-to=justin.restivo@citi.com
+to=justinjrestivo@gmail.com
 from=kernel-checker@citi.com
 kernel_log=/tmp/kernel_log
 
@@ -10,7 +10,16 @@ kernel_log=/tmp/kernel_log
 echo > $kernel_log
 sed -i '1d' $kernel_log
 
-#Validate RHEL release
+if [ -f not-rhel ] && [ -f /etc/redhat-release ] ; then
+	rm not-rhel
+fi
+
+if [ -f not-rhel ]; then
+	echo "System is not RHEL -- exiting."
+	echo "Notification already dispatched to: $to"
+	exit 1
+fi
+
 if [ ! -f /etc/redhat-release ]; then
         echo "System is not RHEL -- exiting."
         touch not-rhel
@@ -18,7 +27,7 @@ if [ ! -f /etc/redhat-release ]; then
         echo "From: $from" >> not-rhel
         echo "Subject: `uname -n` is not a RHEL system" >> not-rhel
         sendmail $to < not-rhel
-        rm not-rhel
+		#rm not-rhel
         exit 1
 fi
 
@@ -34,8 +43,21 @@ rpm -qa | grep -qw sendmail || sudo yum install sendmail
 #This script is self contained and will clean up after itself
 cd "$(dirname "$0")"
 
+yum check-update | grep kernel | grep redhat-release > checker
+
+if [ -f available ] && [ -s checker ] ; then
+	rm available
+	rm checker
+fi
+
+if [ -f available ]; then
+	echo "Kernel update available at $(date)"
+	echo "Notification already dispatched to: $to"
+	exit 1
+fi
+
 #Check for kernel updates available in YUM
-if yum check-update | grep kernel
+if yum check-update | grep kernel | grep redhat-release
 then
 		echo "Kernel update available at $(date)" 2>&1 | tee -a $kernel_log
 		echo >> $kernel_log
@@ -44,7 +66,7 @@ then
 		echo `uname -r` >>$kernel_log
 		echo >> $kernel_log
 		echo "New version(s) of RHEL kernel:" >> $kernel_log
-		yum check-update | grep kernel >> $kernel_log
+		yum check-update | grep kernel | grep redhat-release >> $kernel_log
 		touch available
 		#Build sendmail configuration file
 		echo "To: $to" >> available
@@ -54,7 +76,7 @@ then
 		cat $kernel_log >> available
 		sendmail $to < available
 		#Cleanup
-		rm available
+		#rm available
 else
 		echo "No kernel update available at $(date)" 2>&1 | tee -a $kernel_log
 		echo >> $kernel_log
@@ -69,12 +91,13 @@ else
 		echo "Subject: No RHEL kernel update available on system: `uname -n`" >> not-available
 		echo >> $kernel_log
 		cat $kernel_log >> not-available
-		sendmail $to < not-available
+		#sendmail $to < not-available
 		#Cleanup
 		rm not-available
 fi
 
-clear
+rm checker
+
 cat $kernel_log
 
 exit 0
